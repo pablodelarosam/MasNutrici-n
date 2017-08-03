f02c57ffe5f8faecb2c9cc273a28c327f8459e58
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-08-03 11:59:50 -0500

    Changing structure to +nutricion

---
+++ front/src/dashboard_patient/components/StatusPatient.js
@@ -0,0 +1,90 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import Image from 'grommet/components/Image';
+import {dashboard} from '../actions/dashboard'
+import Split from 'grommet/components/Split';
+import Box from 'grommet/components/Box';
+import Form from 'grommet/components/Form';
+import Paragraph from 'grommet/components/Paragraph';
+import Columns from 'grommet/components/Columns';
+import Anchor from 'grommet/components/Anchor';
+
+export class StatusPatient extends Component {
+
+  render() {
+    const imageCalendar = require('../../static/img/blackCalendar.svg');
+
+    const {children} = this.props;
+    return (
+      <Box>
+        <h1>Estado</h1>
+        <Box direction="row">
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <p>
+              Última visita
+            </p>
+          </Box>
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <p>
+              20/10/2017
+            </p>
+          </Box>
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <Image style={{
+              width: 40
+            }} src={imageCalendar} size="small"/>
+          </Box>
+        </Box>
+
+        <Box direction="row">
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <div className="card">
+
+              <h1 className="headstatus">
+                Grasa
+              </h1>
+              <h1 className="headstatus">
+                200
+              </h1>
+            </div>
+          </Box>
+
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <div className="card">
+
+              <h1 className="headstatus">
+                Masa
+              </h1>
+              <h1 className="headstatus">
+                200
+              </h1>
+            </div>
+          </Box>
+
+          <Box className="box-right-info" pad="medium" basis="1/4">
+            <div className="card">
+
+              <h1 className="headstatus">
+                Cm
+              </h1>
+              <h1 className="headstatus">
+                200
+              </h1>
+            </div>
+          </Box>
+
+        </Box>
+
+        <div className="card">
+
+          <h1 className="headstatus">Peso</h1>
+
+        </div>
+
+      </Box>
+
+    )
+  }
+
+}
+export default connect(null, {dashboard})(StatusPatient)
--- front/src/routes.js
+++ front/src/routes.js
@@ -6,15 +6,14 @@ import {Route, IndexRoute} from 'react-router';
 import Home from "./home/components/Home.js";
 import Login from "./login/components/Login.js";
 import SignIn from "./authentication/components/Signin.js";
-import DashMainStudent from "./dashboard_student/components/DashMain.js";
-import DashStudentExplore from "./dashboard_student/components/DashExplore.js";
-import DashboardExplore from "./dashboard_student/components/DashboardExplore.js";
-import DashboardStudent from "./dashboard_student/components/Dashboard.js";
-import NewsFeedStudent from "./dashboard_student/components/NewsFeed.js";
-import TimeTableStudent from "./dashboard_student/components/TimeTable.js";
-import ProfileStudent from "./dashboard_student/components/Profile.js";
-import CourseStudent from "./dashboard_student/components/Course.js";
-import ClassInfo from "./dashboard_student/components/ClassInfo.js";
+
+import DashMainPatient from "./dashboard_patient/components/DashMain.js";
+import NewsFeedStudent from "./dashboard_patient/components/NewsFeed.js";
+import TimeTableStudent from "./dashboard_patient/components/TimeTable.js";
+import ProfileStudent from "./dashboard_patient/components/Profile.js";
+import CourseStudent from "./dashboard_patient/components/Course.js";
+import StatusPatient from './dashboard_patient/components/StatusPatient.js'
+
 import DashMainTutor from "./dashboard_tutor/components/DashMainTutor.js";
 import DashboardTutor from "./dashboard_tutor/components/DashboardTutor.js";
 import ProfileTutor from "./dashboard_tutor/components/ProfileTutor.js";
@@ -23,23 +22,22 @@ import SessionTutor from "./dashboard_tutor/components/SessionTutor.js";
 
 import AppK from "./App.js"
 
-export const getRoutes = (store) => <Route path="/" component={AppK}>
+export const getRoutes = (store) =>
+<Route path="/" component={AppK}>
   {/* AUTH */}
   <IndexRoute component={Home}/>
   <Route path="/login" component={Login}></Route>
   <Route path="/signin" component={SignIn}></Route>
-  <Route path="/dashboard/student" component={DashMainStudent}>
+
+  <Route path="/dashboard/patient" component={DashMainPatient}>
     <IndexRoute component={NewsFeedStudent}/>
     <Route path="newsfeed" component={NewsFeedStudent}/>
     <Route path="profile" component={ProfileStudent}/>
     <Route path="time" component={TimeTableStudent}/>
     <Route path="course" component={CourseStudent}/>
-    <Route path="explore" component={DashStudentExplore}/>
+    <Route path="status" component={StatusPatient}/>
   </Route>
-  <Route path="/dashboard/student_explore" component={DashStudentExplore}>
-    <IndexRoute component={DashboardExplore}/>
-    <Route path="details" component={ClassInfo}/>
-  </Route>
+
   <Route path="/dashboard/tutor" component={DashMainTutor}>
   <IndexRoute component={NewsFeedStudent}/>
   <Route path="newsfeed" component={NewsFeedStudent}/>

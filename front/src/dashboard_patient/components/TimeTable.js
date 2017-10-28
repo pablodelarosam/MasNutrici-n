b973476ef671cc9450702a309b24e907af445888
gerardodrm <gerardo.delarosama@udlap.mx>
2017-10-28 11:07:48 -0500

    Calendar fix

--- front/src/dashboard_patient/components/TimeTable.js
+++ front/src/dashboard_patient/components/TimeTable.js
@@ -14,12 +14,12 @@ import CheckBox from 'grommet/components/CheckBox';
 import {SingleDatePicker} from 'react-dates';
 import Paragraph from 'grommet/components/Paragraph';
 import ModalGeneral from '../../general_components/ModalGeneral.js';
-//import serializeForm from 'form-serialize';
+import serializeForm from 'form-serialize';
 
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
 BigCalendar.views = ['month', 'day', 'week'];
 
-const format = 'h:mm a';
+const format = 'HH:mm';
 const now = moment().hour(0).minute(0);
 
 export class TimeTablePatient extends Component {
@@ -50,19 +50,30 @@ export class TimeTablePatient extends Component {
     this.setState({"openLayer": false});
   }
   // Get filter changes to modify the query
-  changeOpts(e, type) {
-    // switch (type) {
-    //   case "start_date":
-    //     this.setState({
-    //       "start_date_opt": e
-    //     }, () => this.props.getQueryData(this.state));
-    //     break;
-    //   case "end_date":
-    //     this.setState({
-    //       "end_date_opt": e
-    //     }, () => this.props.getQueryData(this.state));
-    //     break;
-    // }
+  changeOpts(e) {
+    // this.setState({
+    //   "start_date_opt": e
+    // });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var values = serializeForm(e.target, {hash: true});
+    console.log(values);
+    var s_d = moment(values['start_date'] + " " + values['first_time']);
+    var e_d = moment(values['start_date'] + " " + values['first_time']).add('minutes', 20);
+    s_d = s_d.toDate();
+    e_d = e_d.toDate();
+
+    events.push({
+      "title": "New Date",
+      "start": s_d,
+      "end": e_d
+    });
+
+    console.log(events);
+
+    this.onCloseLayer();
   }
 
   render() {
@@ -90,11 +101,11 @@ export class TimeTablePatient extends Component {
           <ModalGeneral title="Agendar cita" showClosed={true} closeDialog={() => this.onCloseLayer()} handleSubmit={(e) => this.handleSubmit(e)}>
             <Box>
             <SingleDatePicker {...defaultProps} id="start_date" date={this.state.start_date_opt} // momentPropTypes.momentObj or null
-                    onDateChange={(e) => this.changeOpts(e, 'start_date')} // PropTypes.func.isRequired
+                    onDateChange={(e) => this.changeOpts(e)} // PropTypes.func.isRequired
                     focused={this.state.focused_start} // PropTypes.bool
                     onFocusChange={({focused}) => this.setState({focused_start: focused})} // PropTypes.func.isRequired
                   />
-              <TimePicker name="timeTo" showSecond={false} defaultValue={now} className="xxx" onChange={this.changeOpts()} format={format} use12Hours/>
+              <TimePicker name="first_time" showSecond={false} defaultValue={now} className="xxx" onChange={this.changeOpts()} format={format} use12Hours/>
             </Box>
           </ModalGeneral>
         )}
